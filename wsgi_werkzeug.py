a734e0dc59dcd17cf645504ce8822c90c492f59e
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-10-12 14:10:57 +0200

    Added support for keyword arguments

--- wsgi_werkzeug.py
+++ wsgi_werkzeug.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import logging
 import os
 
-import logging
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 from django.template import TemplateSyntaxError
 from django.views import debug
 from django.views.debug import technical_500_response
 from django_extensions.management.utils import RedirectHandler
 from werkzeug import DebuggedApplication #pylint: disable=no-name-in-module
-from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
@@ -23,11 +23,11 @@ werklogger.addHandler(RedirectHandler(__name__))
 werklogger.propagate = False
 
 
-def forward_technical_500_response(request, exc_type, exc_value, tb):
+def forward_technical_500_response(request, exc_type, exc_value, tb, **kwargs):
 	if request.META['REMOTE_ADDR'] == '127.0.0.1' and exc_type != TemplateSyntaxError:
 		raise #pylint: disable=misplaced-bare-raise
 	else:
-		return technical_500_response(request, exc_type, exc_value, tb)
+		return technical_500_response(request, exc_type, exc_value, tb, **kwargs)
 
 
 debug.technical_500_response = forward_technical_500_response
